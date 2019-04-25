93a59eba24a22ede31646cfcad1443ced03463ab
rafael.chevez <rafaelchevez4@gmail.com>
2019-04-25 17:06:45 -0600

    added retrofit file

---
+++ app/src/main/java/com/example/appjob/iRetrofit/PersonService.java
@@ -0,0 +1,29 @@
+package com.example.appjob.iRetrofit;
+
+import com.example.appjob.model.Person;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface PersonService {
+
+
+    @POST("add")
+    Call<Person> insert_person(@Body Person objPet);
+
+    @GET("show")
+    Call<List<Person>>list();
+
+    @DELETE("delete/{id}")
+    Call<Person>delete_person(@Path("id") int id);
+
+    @PUT("update/{id}")
+    Call<Person>update_person(@Path("id") int id,@Body Person objPet);
+}
--- app/src/main/java/com/example/appjob/retrofitRemote/ApiUtils.java
+++ app/src/main/java/com/example/appjob/retrofitRemote/ApiUtils.java
@@ -1,4 +1,14 @@
 package com.example.appjob.retrofitRemote;
 
+import com.example.appjob.iRetrofit.PersonService;
+import com.example.appjob.model.Person;
+
 public class ApiUtils {
+
+    public static final String API_URL = "http://192.168.100.1:8080/pet/";
+
+
+    public static PersonService getUserService(){
+        return RetrofitClient.getClient(API_URL).create(PersonService.class);
+    }
 }
