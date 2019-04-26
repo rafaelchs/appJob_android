b2b60a9f5f059fe2724feb0ae77dbb11fe4f9fa7
rafael.chevez <rafaelchevez4@gmail.com>
2019-04-26 16:18:44 -0600

    added function login

---
+++ app/src/main/java/com/example/appjob/model/RestObj.java
@@ -0,0 +1,17 @@
+package com.example.appjob.model;
+
+public class RestObj {
+
+    private String message;
+
+    public RestObj() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
